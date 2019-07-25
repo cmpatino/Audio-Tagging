35c420424e523f06b21826ab6f1a70990be644ed
carlosmiguelpatino <cm.patino10@uniandes.edu.co>
2019-07-25 11:34:27 -0500

    save history and raw predictions files

--- scripts/train.py
+++ scripts/train.py
@@ -1,9 +1,10 @@
 from sklearn.model_selection import train_test_split
-from keras.callbacks import ModelCheckpoint
+from tensorflow.keras.callbacks import ModelCheckpoint
 import os
 import shutil
 import numpy as np
 import pandas as pd
+import pickle
 
 import data_utils as du
 import models
@@ -11,7 +12,8 @@ import models
 DATA_PATH = '../data/input/'
 
 
-def train_with_val(model_name, data_config, model_config):
+def train_with_val(model_name, data_config,
+                   model_config, make_submission=True):
 
     train_df = du.create_train_df(DATA_PATH)
 
@@ -31,20 +33,18 @@ def train_with_val(model_name, data_config, model_config):
                                             DATA_PATH,
                                             data_config)
 
-    #map3_callback = Map3Callback(train_generator, train_set.label_idx,
-    #                             val_generator, val_set.label_idx,
-    #                             train_df.label_idx)
     directory = f'../models/{model_name}/'
     if os.path.exists(directory):
-        replace = input(f'Do you want to replace the current {directory} dir? [Y/N]')
+        replace = input(f'Do you want to replace the current \
+                        {directory} dir? [Y/N]\n')
         if replace == 'N':
             raise FileExistsError('Directory already exists')
-        else: 
+        else:
             shutil.rmtree(directory)
             os.mkdir(directory)
     else:
         os.mkdir(directory)
-    checkpoint = ModelCheckpoint(directory + 'best.h5',
+    checkpoint = ModelCheckpoint(directory + 'best_train_val.h5',
                                  monitor='val_loss',
                                  verbose=1, save_best_only=True)
     history = model.fit_generator(train_generator,
@@ -53,8 +53,30 @@ def train_with_val(model_name, data_config, model_config):
                                   use_multiprocessing=True, workers=2,
                                   max_queue_size=20,
                                   callbacks=[checkpoint])
-    #history.history['map3_val'] = map3_callback.map3_val
-    #history.history['map3_train'] = map3_callback.map3_train
+
+    with open(directory + 'history_train_val.pkl', 'wb') as f:
+        pickle.dump(history.history, f)
+
+    if make_submission:
+
+        test_generator = du.make_pred_generator(DATA_PATH,
+                                                data_config)
+
+        predictions = model.predict_generator(test_generator, verbose=1)
+
+        # Save predictions
+        np.save(directory + "raw_train_val_predictions.npy", predictions)
+
+        # Make a submission file
+        train_df = du.create_train_df(DATA_PATH)
+        test_df = pd.read_csv(DATA_PATH + 'sample_submission.csv')
+        test_df = test_df[['fname']].copy()
+        labels = list(train_df.label.unique())
+        top_3 = np.array(labels)[np.argsort(-predictions, axis=1)[:, :3]]
+        predicted_labels = [' '.join(list(x)) for x in top_3]
+        test_df['label'] = predicted_labels
+        test_df.to_csv(f'../submissions/submission_train_val_{model_name}.csv',
+                       index=False)
 
 
 def train_for_submission(model_name, data_config, model_config):
@@ -78,6 +100,9 @@ def train_for_submission(model_name, data_config, model_config):
                                   use_multiprocessing=True, workers=2,
                                   max_queue_size=20)
 
+    with open(directory + 'history_full.pkl', 'wb') as f:
+        pickle.dump(history.history, f)
+
     model.save_weights(directory + 'best_for_submission.h5')
 
 
@@ -87,7 +112,7 @@ def make_submission(model_name):
 
     data_config = du.DataConfig()
     train_for_submission(model_name, data_config, model_config)
-    
+
     try:
         model = getattr(models, f'get_{model_name}')(model_config)
     except AttributeError:
@@ -99,6 +124,9 @@ def make_submission(model_name):
 
     predictions = model.predict_generator(test_generator, verbose=1)
 
+    # Save predictions
+    np.save(directory + "raw_predictions.npy", predictions)
+
     # Make a submission file
     train_df = du.create_train_df(DATA_PATH)
     test_df = pd.read_csv(DATA_PATH + 'sample_submission.csv')
@@ -107,12 +135,13 @@ def make_submission(model_name):
     top_3 = np.array(labels)[np.argsort(-predictions, axis=1)[:, :3]]
     predicted_labels = [' '.join(list(x)) for x in top_3]
     test_df['label'] = predicted_labels
-    test_df.to_csv(f'../submissions/submission_{model_name}.csv', index=False)
+    test_df.to_csv(f'../submissions/submission_full_{model_name}.csv',
+                   index=False)
 
 
-model_name = 'dummy_model'
+model_name = 'baseline_model'
 data_config = du.DataConfig()
-model_config = models.ModelConfig(max_epochs=2)
+model_config = models.ModelConfig(max_epochs=1)
 
-train_with_val(model_name, data_config, model_config)
-#make_submission(model_name)
+train_with_val(model_name, data_config, model_config, make_submission=True)
+# make_submission(model_name)
